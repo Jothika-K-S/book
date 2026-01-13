13b0820803dbd75d1509e33317e7b19ae0359469
Jothika <123849832+Jothika-K-S@users.noreply.github.com>
2026-01-13 21:59:51 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,15 +12,15 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(cors())
 
-// app.use(
-//     cors(
-//         {
-//             origin: 'http://localhost:1234/',
-//             methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//             allowedHeaders: ['Content-Type', 'Authorization'],
-//         }
-//     )
-// )
+app.use(
+    cors(
+        {
+            origin: 'http://localhost:1234/',
+            methods: ['GET', 'POST', 'PUT', 'DELETE'],
+            allowedHeaders: ['Content-Type', 'Authorization'],
+        }
+    )
+)
 
 app.use('/api/books',bookRoute)
 
