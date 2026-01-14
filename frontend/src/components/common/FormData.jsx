c859537546d82e30c8e57af7c936792c8920b85f
Jothika <123849832+Jothika-K-S@users.noreply.github.com>
2026-01-14 11:25:18 +0530

    Update FormData.jsx

--- frontend/src/components/common/FormData.jsx
+++ frontend/src/components/common/FormData.jsx
@@ -18,7 +18,7 @@ const FormData = () => {
         const updateBook = async() => {
         try{
             if(id){
-                const editBook = await fetch(`https://appsail-50037828679.development.catalystappsail.in/api/books/${id}`)
+                const editBook = await fetch(`https://bookmanagement.development.catalystappsail.in/api/books/${id}`)
                 const {title, author,description,image,genre,rating,buy} = await editBook.json()
                 setTitle(title)
                 setAuthor(author)
@@ -48,9 +48,9 @@ const FormData = () => {
         }
        try{
         if(id){
-           return axios.put(`https://appsail-50037828679.development.catalystappsail.in/api/books/${id}`, bookData)
+           return axios.put(`https://bookmanagement.development.catalystappsail.in/api/books/${id}`, bookData)
         }
-        return axios.post('https://appsail-50037828679.development.catalystappsail.in/api/books', bookData)
+        return axios.post('https://bookmanagement.development.catalystappsail.in/api/books', bookData)
        }catch(err){
         console.log(err)
        }
